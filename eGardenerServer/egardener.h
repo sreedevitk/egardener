217f5b5e0941d6c8ae39fcb5ddb2e21f89246375
sreedevitk <sreedevitk@gmail.com>
2017-08-22 14:07:04 +0530

    commit for egardener server

---
+++ eGardenerServer/egardener.h
@@ -0,0 +1,30 @@
+const char MAIN_PAGE[] PROGMEM = R"=====(
+<HTML>
+  <HEAD>
+      <TITLE>My first web page</TITLE>
+  </HEAD>
+<BODY>
+  <CENTER>
+      <B>Hello World..Not sure that it is working ? </B>
+  </CENTER> 
+</BODY>
+</HTML>
+)=====";
+
+
+/*
+char M1[] PROGMEM = R"=====(
+<HTML>
+	<HEAD>
+			<TITLE>My first web page</TITLE>
+	</HEAD>
+<BODY>
+	<CENTER>
+			<B>Hello World..Not sure that it is working ? </B>
+     )=====";
+char M2[] PROGMEM= R"=====(     
+	</CENTER>	
+</BODY>
+</HTML>
+)=====";
+*/
